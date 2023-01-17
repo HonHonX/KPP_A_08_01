9ff9789ba24f476f3d6be0d288529e876ec052c0
Sherry <119943082+HonHonX@users.noreply.github.com>
2023-01-17 13:13:23 +0100

    Update food.c

--- food.c
+++ food.c
@@ -23,8 +23,8 @@ int main () {
 	
 	//Ziel ist, eine Liste mit folgenden Werten auszugeben: Name, Packungsgröße, Kilopreis netto in Euro, Kilopreis brutto in Euro(7% Mwst.)
 	for (int i=0; i<5; i++) {
-		float netto = preis[i]/packungsgroesse[i]*1000;
-		float brutto = netto*1.07;
+		float netto = brutto/107*100
+		float brutto = preis[i]/packungsgroesse[i]*1000;
 		printf("\nProdukt[%d]: %s | %dg pro Einheit | %.2f€/kg(netto) | %.2f€/kg(brutto)",i+1,name[i],packungsgroesse[i],netto,brutto);	
 		
 		////////////////////////////////////////////////////////////////////////////
